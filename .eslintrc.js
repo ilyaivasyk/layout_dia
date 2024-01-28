f9ea128fab353771081b189162cccaeb34a9e6cf
Illia <ilyaivasyk1@gmail.com>
2024-01-28 14:57:59 +0200

    make build

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: '@mate-academy/eslint-config',
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true,
-    "jquery": true
-  }
-};
---
+++ index.7f280d43.js
@@ -0,0 +1,3 @@
+var e=jQuery.noConflict();e(document).ready(function(){e(".slick").slick({settings:{infinite:!0,adaptiveHeight:!0,adaptiveWidth:!0,dots:!0,arrows:!0,draggable:!1,lazyLoad:"eager",slidesToShow:1,slidesToScroll:1}})}),document.querySelectorAll(".menu").forEach(function(e){e.addEventListener("wheel",function(e){e.preventDefault()})});//# sourceMappingURL=index.7f280d43.js.map
+
+//# sourceMappingURL=index.7f280d43.js.map
